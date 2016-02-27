610e6bb5879d8b74c772021a7b6b4f2f3d246bca
Andrew Baron <Drowelfofdead@gmail.com>
2016-02-27 13:48:10 -0500

    delete button working

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,11 +9,6 @@ $(function() {
     var editStorageValues = JSON.parse(localStorage.getItem('editStorage'));
 
 
-    $('.appointments-holder').append("<a href='indexEditAppointment.html'><div class='mockup-appointment-holder'><div class='left-col'><div class='appt-time'>" + storageValues.time + "</div><i class='fa fa-sun-o'></i></div><div class='right-col'><div class='appt-title'>" + "&nbsp;" + storageValues.title + "</div><span class='city-state'>" + "&nbsp;" + storageValues.city + "</span></div><div class = 'invisStreet'>" + storageValues.street + "</div><div class = 'invisDate'>" + storageValues.date + "</div></div> </a>")
-
-
-
-
     $('.save-button').on("click", function() {
         storage.title = $('.input-title').val();
         storage.street = $('.input-address').val();
@@ -22,6 +17,13 @@ $(function() {
         storage.time = $('.input-time').val();
 
         localStorage.setItem('storage', JSON.stringify(storage));
+    });
+
+
+    $('.appointments-holder').append("<a href='indexEditAppointment.html'><div class='mockup-appointment-holder'><div class='left-col'><div class='appt-time'>" + storageValues.time + "</div><i class='fa fa-sun-o'></i></div><div class='right-col'><div class='appt-title'>" + "&nbsp;" + storageValues.title + "</div><span class='city-state'>" + "&nbsp;" + storageValues.city + "</span></div><div class = 'invisStreet'>" + storageValues.street + "</div><div class = 'invisDate'>" + storageValues.date + "</div></div> </a>")
+
+    $('.delete-button').on('click', function() {
+        localStorage.clear();
     });
 
 
@@ -35,9 +37,12 @@ $(function() {
     });
 
 
+
     $('.edit-input-title')[0].placeholder = editStorageValues.title;
     $('.edit-input-address')[0].placeholder = editStorageValues.street;
     $('.edit-input-city')[0].placeholder = editStorageValues.city;
     $('.edit-input-date')[0].placeholder = editStorageValues.date;
     $('.edit-input-time')[0].placeholder = editStorageValues.time;
+
+
 });
