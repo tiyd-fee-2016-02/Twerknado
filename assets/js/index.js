b658e4eeb55c71292aa95837e0a81eaa992ef440
Andrew Baron <Drowelfofdead@gmail.com>
2016-02-27 12:14:05 -0500

    edit appointment populates with correct info

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,12 +8,8 @@ $(function() {
     var storageValues = JSON.parse(localStorage.getItem('storage'));
     var editStorageValues = JSON.parse(localStorage.getItem('editStorage'));
 
-    // console.log(storageValues);
 
-    // var test = storageValues.title;
-    // console.log(test);
-
-    $('.appointments-holder').append("<a href='indexEditAppointment.html'><div class='mockup-appointment-holder'><div class='left-col'><div class='appt-time'>" + storageValues.time + "</div><i class='fa fa-sun-o'></i></div><div class='right-col'><div class='appt-title'>" + "&nbsp;" + storageValues.title + "</div><span class='city-state'>" + "&nbsp;" + storageValues.city + "</span></div></div> </a>")
+    $('.appointments-holder').append("<a href='indexEditAppointment.html'><div class='mockup-appointment-holder'><div class='left-col'><div class='appt-time'>" + storageValues.time + "</div><i class='fa fa-sun-o'></i></div><div class='right-col'><div class='appt-title'>" + "&nbsp;" + storageValues.title + "</div><span class='city-state'>" + "&nbsp;" + storageValues.city + "</span></div><div class = 'invisStreet'>" + storageValues.street + "</div><div class = 'invisDate'>" + storageValues.date + "</div></div> </a>")
 
 
 
@@ -25,38 +21,23 @@ $(function() {
         storage.date = $('.input-date').val();
         storage.time = $('.input-time').val();
 
-        // console.log(storage);
         localStorage.setItem('storage', JSON.stringify(storage));
     });
 
 
-
     $('.mockup-appointment-holder').on("click", function() {
-      console.log($(this).find('.appt-title')[0].innerHTML);
-      editStorage.title = ($(this).find('.appt-title')[0].innerHTML);
-      editStorage.street = ($(this).find('.street')[0].innerHTML);
-      editStorage.city = ($(this).find('.city-state')[0].innerHTML);
-      editStorage.date = ($(this).find('.appt-date')[0].innerHTML);
-      editStorage.time = ($(this).find('.appt-time')[0].innerHTML);
-
-      console.log(editStorage);
+      editStorage.title = ($(this).find('.appt-title')[0].innerText);
+      editStorage.street = ($(this).find('.invisStreet')[0].innerText);
+      editStorage.city = ($(this).find('.city-state')[0].innerText);
+      editStorage.date = ($(this).find('.invisDate')[0].innerText);
+      editStorage.time = ($(this).find('.appt-time')[0].innerText);
       localStorage.setItem('editStorage', JSON.stringify(editStorage));
-
     });
 
-    //
-    //
-    // for(var i=0; i<localStorage.length; i++){
-    //   if (localStorage.length === 0){
-    //     localStorage.setItem('storage', JSON.stringify(storage));
-    //   }
-    //   else {
-    //    storage[i] = storage;
-    //    localStorage.setItem('storage[i]', JSON.stringify(storage[i]));
-    //    console.log(localStorage);
-    //   }
-    // }
-    //
 
-
+    $('.edit-input-title')[0].placeholder = editStorageValues.title;
+    $('.edit-input-address')[0].placeholder = editStorageValues.street;
+    $('.edit-input-city')[0].placeholder = editStorageValues.city;
+    $('.edit-input-date')[0].placeholder = editStorageValues.date;
+    $('.edit-input-time')[0].placeholder = editStorageValues.time;
 });
